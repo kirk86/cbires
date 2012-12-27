37ccc2add89a6a79366bc8f0cca844f5ea1b1217
kirk86 <kirk86@walla.com>
2012-12-27 12:28:10 +0200

    initial commit

--- db/DB.php
+++ db/DB.php
@@ -13,129 +13,163 @@
 
 class DB
 {
+    // Hold instance of PDO wrapper
     private static $instance;
-    
-    private function __construct()
-    {
-        
-    }
-    
+
+    /**
+     * Prevent direct creation of object
+     * 
+     * @param empty
+     * @return empty
+     */
+    private function __construct() { }
+
+    /**
+     * Create new PDO instance
+     * 
+     * @param empty
+     * @return object, PDO instance
+     */
     private static function getInstance()
     {
-        if(!isset(self::$instance))
+        if (!isset(self::$instance))
         {
             try
             {
                 self::$instance = new PDO(PDO_DSN);
-                
+
                 // Configure PDO to throw exceptions
-				self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                
+                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
             }
-            catch(PDOException $e)
+            catch (PDOException $e)
             {
                 // Close the database handler and trigger an error
-				self::Close();
-				trigger_error($e->getMessage(), E_USER_ERROR);
+                self::Close();
+                trigger_error($e->getMessage(), E_USER_ERROR);
             }
         }
-        
+
         return self::$instance;
     }
-    
-    public static function Close()
+
+    /**
+     * Close db connection
+     * 
+     * @param empty
+     * @return empty
+     */
+    public static function close()
     {
         self::$instance = null;
     }
-    
-    //Execute queries such as INSERT,UPDATE
-    public static function Execute($query, $params = null)
+
+    /**
+     * Execute queries such as INSERT,UPDATE
+     * Wrapper method for PDOStatement::execute()
+     * 
+     * @param string $query SQL query
+     * @param array $params parameters for prepared named SQL statements
+     * @return string affected rows
+     */
+    public static function execute($query, $params = null)
     {
         try
         {
-            $db = self::getInstance();
-            
+            $db        = self::getInstance();
             $statement = $db->prepare($query);
-            
             $statement->execute($params);
-            
+
         }
-        catch(PDOException $e)
+        catch (PDOException $e)
         {
             self::Close();
             trigger_error($e->getMessage(), E_USER_ERROR);
         }
-        
-        return $statement->rowCount(); 
+
+        return $statement->rowCount();
     }
-    
-    public static function GetAll($query, $params = null, $fetchStyle = PDO::FETCH_ASSOC)
-    {
-        $result = null;
-        
-        try
-        {
-            $db = self::getInstance();
-            
-            $statement = $db->prepare($query);
-              
-            $statement->execute($params);
-            
-            $result = $statement->fetchAll($fetchStyle);
-        }
-        catch(PDOException $e)
-        {
-            self::Close();
-            trigger_error($e->getMessage(), E_USER_ERROR);
-        }
-        
-        return $result;
-    }
-    
-    public static function GetRow($query, $params = null, $fetchStyle = PDO::FETCH_ASSOC)
+
+    /**
+     * Wrapper method for PDOStatement::fetchAll()
+     * 
+     * @param string $query SQL query
+     * @param array $params parameteres for prepared named SQL statements
+     * @param constant $fetchStyle fetch mode for PDO
+     */
+    public static function getAll($query, $params = null, $fetchStyle = PDO::FETCH_ASSOC)
     {
         $result = null;
 
         try
         {
-            $db = self::getInstance();
-            
+            $db        = self::getInstance();
             $statement = $db->prepare($query);
-            
             $statement->execute($params);
-            
+            $result    = $statement->fetchAll($fetchStyle);
+                 
+        }
+        catch (PDOException $e) {
+            self::Close();
+            trigger_error($e->getMessage(), E_USER_ERROR);
+        }
+
+        return $result;
+        //return $clean_result;
+    }
+    
+    /**
+     * Wrapper method for PDOStatement::fetch()
+     * 
+     * @param string $query SQL query
+     * @param array $params parameteres for prepared named SQL statements
+     * @param constant $fetchStyle fetch mode for PDO
+     */
+    public static function getRow($query, $params = null, $fetchStyle = PDO::FETCH_ASSOC)
+    {
+        $result = null;
+
+        try {
+            $db = self::getInstance();
+
+            $statement = $db->prepare($query);
+
+            $statement->execute($params);
+
             $result = $statement->fetch($fetchStyle);
         }
-        catch(PDOException $e)
-        {
+        catch (PDOException $e) {
             self::Close();
             trigger_error($e->getMessage(), E_USER_ERROR);
         }
-        
+
         return $result;
     }
     
-    //return the first column value from a row.
-    public static function GetOne($query, $params = null)
+    /**
+     * Return the first column value from a row
+     * 
+     * @param string $query SQL query
+     * @param array $params parameteres for prepared named SQL statements
+     */
+    public static function getOne($query, $params = null)
     {
         $result = null;
-        
-        try
-        {
-           $db = self::getInstance();
 
-           $statement = $db->prepare($query);
-           
-           $statement->execute($params);
-           
-           $result = $statement->fetch(PDO::FETCH_NUM); 
+        try {
+            $db = self::getInstance();
+
+            $statement = $db->prepare($query);
+
+            $statement->execute($params);
+
+            $result = $statement->fetch(PDO::FETCH_NUM);
         }
-        catch(PDOException $e)
-        {
+        catch (PDOException $e) {
             self::Close();
             trigger_error($e->getMessage(), E_USER_ERROR);
         }
-        
+
         return $result;
     }
 }
