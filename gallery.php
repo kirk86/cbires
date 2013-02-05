34cc886ca5baeebc7ecf4fd6c552821cd09a2dc2
kirk86 <kirk86@walla.com>
2013-02-05 03:10:34 +0200

    minor bug fixes, added hsv option for rocchio

--- gallery.php
+++ gallery.php
@@ -38,13 +38,14 @@
                             $_SESSION['threshold']        = Tools::getValue('topk');
                             $_SESSION['distanceFunction'] = Tools::getValue('distanceFunction');
                             $_SESSION['colorSpace']       = Tools::getValue('colorSpace');
+                            unset($_SESSION['rgb_histogram']);
+                            unset($_SESSION['hsv_histogram']);
                             session_write_close();
                             echo MESSAGE_SUCCESS_SETTINGS;
                         }
                     }					
                     if (Tools::isSubmit('submit') && Tools::getIsset('submit'))
                     {
-                        echo "test 1 <br />";
 						/* rocchio algorimthm start */
 
 						$sum_nr = 0;
@@ -52,24 +53,17 @@
 						$sum_image_histogram_sum = array_fill(0, 64, '0');
 						$difference_image_histogram_sum = array_fill(0, 64, '0');
                         
-                        //var_dump($_POST);
-                        //var_dump($_SESSION);
-                        //echo $_SESSION['rgb_histogram'];
 						foreach ($_POST as $key => $id) :
 							if($key != "submit")
 							{
-		                          echo "test 2 <br />";
-                                  //var_dump($_SESSION);
                                 if (isset($_SESSION['rgb_histogram']) && !empty($_SESSION['rgb_histogram']))
                                 {
-                                    echo "rgb query <br />";
                                     $sql_image = "SELECT array_to_json(color_histogram) AS image_histogram 
 											  FROM tbl_image
 											  WHERE id_image = ".$key;
                                 }
                                 if (isset($_SESSION['hsv_histogram']) && !empty($_SESSION['hsv_histogram']))
                                 {
-                                    echo "hsv query <br />";
                                     $sql_image = "SELECT array_to_json(hsv_histogram) AS image_histogram 
 											  FROM tbl_image
 											  WHERE id_image = ".$key;
@@ -78,13 +72,11 @@
 								$image_histogram = json_decode($qresult_image[0]['image_histogram']);				
 								if($id == 1)
 								{
-								    echo "test 3 <br />";
 									$sum_image_histogram_sum = DistanceMetrics::sumVectors($image_histogram, $sum_image_histogram_sum);
 									$sum_nr++;
 								}
 								elseif($id == -1)
 								{
-								    echo "test 4 <br />";
 									$difference_image_histogram_sum = DistanceMetrics::sumVectors($image_histogram, $difference_image_histogram_sum);
 									$difference_nr++;
 								}
@@ -94,40 +86,27 @@
 												
 						if($sum_nr != 0)
 						{
-						  echo "test 5 <br />";
 							for ($i = 0; $i < count($sum_image_histogram_sum); $i++)
 							{
 								$sum_image_histogram_sum[$i] = $sum_image_histogram_sum[$i] / $sum_nr * 0.75;
 							}
 						}
 						
-						//echo "<pre>sum_image_histogram_sum<br />";
-						//print_r($sum_image_histogram_sum);
-						//echo "</pre>";
-						//echo $sum_nr;
-						
 						if($difference_nr != 0)
 						{
-						  echo "test 6 <br />";
 							for ($i = 0; $i < count($difference_image_histogram_sum); $i++)
 							{
 								$difference_image_histogram_sum[$i] = $difference_image_histogram_sum[$i] / $difference_nr * 0.25;
 							}
 						}
-						
-						//echo "<pre>difference_image_histogram_sum<br />";
-						//print_r($difference_image_histogram_sum);
-						//echo "</pre>";
-						//echo $difference_nr;
 
-						if(isset($_SESSION['rgb_histogram']))
+						if(isset($_SESSION['rgb_histogram']) || isset($_SESSION['hsv_histogram']))
 						{
-						  echo "test 7 <br />";
-							$original_image_histogram = $_SESSION['rgb_histogram'];
-							
-							//echo "<pre>original_image_histogram<br />";
-							//print_r($original_image_histogram);
-							//echo "</pre>";
+                            if (!empty($_SESSION['rgb_histogram']))
+                                $original_image_histogram = $_SESSION['rgb_histogram'];
+                            elseif (!empty($_SESSION['hsv_histogram']))
+                                $original_image_histogram = $_SESSION['hsv_histogram'];
+
 							
                             $query_image_histogram_sum = array_fill(0, 64, 0);
 							//$query_image_histogram_sum = 0;
@@ -135,13 +114,14 @@
 							$query_image_histogram_sum = DistanceMetrics::sumVectors($sum_image_histogram_sum, $query_image_histogram_sum);
                             //$query_image_histogram_sum = DistanceMetrics::sumVectors($sum_image_histogram_sum, $original_image_histogram);
 							$query_image_histogram_sum = DistanceMetrics::diffVectors($query_image_histogram_sum, $difference_image_histogram_sum);
-							
-							//echo "<pre>query_histogram<br />";
-							//print_r($query_image_histogram_sum);
-							//echo "</pre>";
 
 							// uncoment this if you want to requery multiple times
-							$_SESSION['rgb_histogram'] = $query_image_histogram_sum;
+                            if (!empty($_SESSION['rgb_histogram']))
+                                $_SESSION['rgb_histogram'] = $query_image_histogram_sum;
+                            elseif (!empty($_SESSION['hsv_histogram']))
+                                $_SESSION['hsv_histogram'] = $query_image_histogram_sum;
+                            
+                            session_write_close();
 						}
 						/* rocchio algorimthm end */
 						
@@ -152,7 +132,6 @@
                              !empty( $_FILES['fileInput']['type'] ) && 
                              !empty( $_FILES['fileInput']['tmp_name'] ) )
                         {
-                            echo "test 8 <br />";
                             $tempFile = $_FILES['fileInput']['tmp_name'];
                             $targetPath = $_SERVER['DOCUMENT_ROOT'] . $targetFolder;
                             $galleryPath = $_SERVER['DOCUMENT_ROOT'] . $galleryFolder;
@@ -163,7 +142,6 @@
                             
                             if ( in_array( Tools::strtolower( $fileParts['extension'] ), $fileTypes ) )
                             {
-                                echo "test 9 <br />";
                                 $img = new Image($tempFile);
                                 $img->save($galleryPath, false);
                                 $img->resize(300, 0); // Lower quality image created using width ratio otherwise $img->resize(300, 0, false); for quality not speed
@@ -176,16 +154,15 @@
                                      isset($_SESSION['threshold'])        && !empty($_SESSION['threshold'])
                                    )
                                 {
-                                    echo "test 10 <br />";
                                     switch ($_SESSION['colorSpace'])
                                     {
                                         case 'RGB':
 										{
-										  echo "test 11 <br />";
 											$objRGB      = new Histogram($resized_img);
 											$histoRGB    = $objRGB->generateHistogram();
 											$normHistRGB = DistanceMetrics::computeHistogram($histoRGB, 64, min($histoRGB), max($histoRGB));
 											$_SESSION['rgb_histogram'] = $normHistRGB;
+                                            unset($_SESSION['hsv_histogram']);
                                             session_write_close();
 											$meanRGB     = DistanceMetrics::mean($normHistRGB);
 											$stdRGB      = DistanceMetrics::std($normHistRGB);
@@ -200,12 +177,12 @@
                                         
                                         case 'HSV':
 										{
-										  echo "test 12 <br />";
 											$objHSV      = new Histogram($resized_img);
 											$histoHSV    = $objHSV->generateHistogram(true);
 											$binHistHSV  = DistanceMetrics::computeHistogram($histoHSV, 64, min($histoHSV), max($histoHSV), false);
 											$normHistHSV = DistanceMetrics::normalize($binHistHSV);
 											$_SESSION['hsv_histogram'] = $normHistHSV;
+                                            unset($_SESSION['rgb_histogram']);
                                             session_write_close();
 											$meanHSV     = DistanceMetrics::mean($normHistHSV);
 											$stdHSV      = DistanceMetrics::std($normHistHSV);
@@ -221,11 +198,11 @@
                                 }
                                 else 
 								{
-								    echo "test 13 <br />";
 									$objRGB      = new Histogram($resized_img);
 									$histoRGB    = $objRGB->generateHistogram();
 									$normHistRGB = DistanceMetrics::computeHistogram($histoRGB, 64, min($histoRGB), max($histoRGB));
 									$_SESSION['rgb_histogram'] = $normHistRGB;
+                                    unset($_SESSION['hsv_histogram']);
                                     session_write_close();
 									$meanRGB     = DistanceMetrics::mean($normHistRGB);
 									$stdRGB      = DistanceMetrics::std($normHistRGB);
@@ -239,50 +216,53 @@
 							}
                         	else
 							{
-							 echo "test 14 <br />";
 								echo MESSAGE_INVALID_FILE_TYPE;
 							}
                         }
 						else
 						{
-						  echo "test 15 <br />";
 							//$combRGB = PopulateImages::computeRgbImages($query_image_histogram_sum);
 							if ( isset($_SESSION['colorSpace'])       && !empty($_SESSION['colorSpace'])       &&
-                                     isset($_SESSION['distanceFunction']) && !empty($_SESSION['distanceFunction']) &&
-                                     isset($_SESSION['threshold'])        && !empty($_SESSION['threshold'])
+                                 isset($_SESSION['distanceFunction']) && !empty($_SESSION['distanceFunction']) &&
+                                 isset($_SESSION['threshold'])        && !empty($_SESSION['threshold'])
                                    )
 							{
-							 echo "test 16 <br />";
 								switch ($_SESSION['colorSpace'])
 								{
 									case 'RGB':
 									{
-									   echo "test 17 <br />";
-										$combRGB = PopulateImages::computeRgbImages($query_image_histogram_sum);
-										
-										// Retrieval Results
-										PopulateImages::retrievalResults($combRGB);
-										
+                                        if ( empty( $_FILES['fileInput']['name'] ) && empty( $_FILES['fileInput']['type'] ) && 
+                                             empty( $_FILES['fileInput']['tmp_name']) && Tools::getValue('submit') != "relevance_feedback" )
+                                             echo MESSAGE_EMPTY_FILE_UPLOAD;
+                                        else
+                                        {                                         
+                                            $combRGB = PopulateImages::computeRgbImages($query_image_histogram_sum);
+                                            // Retrieval Results
+                                            PopulateImages::retrievalResults($combRGB);
+                                        }										
 										break;
 									}
 									
 									case 'HSV':
 									{
-									   echo "test 18 <br />";
-										$normHistHSV = DistanceMetrics::normalize($query_image_histogram_sum);
-										$combHSV = PopulateImages::computeHsvImages($normHistHSV);
-										
-										// Retrieval Results
-										PopulateImages::retrievalResults($combHSV);
-						
+                                        if ( empty( $_FILES['fileInput']['name'] ) && empty( $_FILES['fileInput']['type'] ) && 
+                                             empty( $_FILES['fileInput']['tmp_name']) && Tools::getValue('submit') != "relevance_feedback" )
+                                             echo MESSAGE_EMPTY_FILE_UPLOAD;
+                                        else
+                                        {
+                                            $normHistHSV = DistanceMetrics::normalize($query_image_histogram_sum);
+                                            $combHSV = PopulateImages::computeHsvImages($normHistHSV);
+                                            // Retrieval Results
+                                            PopulateImages::retrievalResults($combHSV);
+                                        }						
 										break;
 									}
 								}
 							}
 							else 
 							{
-							     echo "test 19 <br />";
-    							 if ( empty( $_FILES['fileInput']['name'] ) && empty( $_FILES['fileInput']['type'] ) && empty( $_FILES['fileInput']['tmp_name'] ))
+    							 if ( empty( $_FILES['fileInput']['name'] ) && empty( $_FILES['fileInput']['type'] ) && 
+                                      empty( $_FILES['fileInput']['tmp_name'] ) && Tools::getValue('submit') != "relevance_feedback" )
                                     echo MESSAGE_EMPTY_FILE_UPLOAD;
                                  else
                                  {
